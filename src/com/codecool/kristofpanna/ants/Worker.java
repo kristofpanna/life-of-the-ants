2f9b730ad4e2e641327c6464685371830359df7a
Panna Kristóf <kristofpanna@gmail.com>
2019-09-13 23:39:47 +0200

    Delete unused imports

--- src/com/codecool/kristofpanna/ants/Worker.java
+++ src/com/codecool/kristofpanna/ants/Worker.java
@@ -2,9 +2,6 @@ package com.codecool.kristofpanna.ants;
 
 import com.codecool.kristofpanna.util.Direction;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 class Worker extends Ant {
     public Worker(int gridSize) {
--- src/com/codecool/kristofpanna/util/Direction.java
+++ src/com/codecool/kristofpanna/util/Direction.java
@@ -1,7 +1,5 @@
 package com.codecool.kristofpanna.util;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
