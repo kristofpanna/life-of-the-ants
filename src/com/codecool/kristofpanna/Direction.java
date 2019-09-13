60a2622faf8815603e830a3ac6fc89dc53d1a03d
Panna Kristóf <kristofpanna@gmail.com>
2019-09-13 19:36:56 +0200

    Add Direction enum (for possible directions on the grid)

---
+++ src/com/codecool/kristofpanna/Direction.java
@@ -0,0 +1,24 @@
+package com.codecool.kristofpanna;
+
+/**
+ * Directions on the 2D grid.
+ */
+public enum Direction {
+    NORTH(0,1),  EAST(1,0), SOUTH(-1, 0), WEST(0, -1);
+
+    private int x;
+    private int y;
+
+    Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
