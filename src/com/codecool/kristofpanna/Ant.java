7f3a1a9d5a8b56eecbccb694c4b13e6e4f0e2e46
Panna Kristóf <kristofpanna@gmail.com>
2019-09-13 20:00:01 +0200

    Add members to Ant with default implementations

--- src/com/codecool/kristofpanna/Ant.java
+++ src/com/codecool/kristofpanna/Ant.java
@@ -1,4 +1,48 @@
 package com.codecool.kristofpanna;
 
 abstract class Ant {
+    /**
+     * Max absolute value of coordinates on the grid the ant lives on.
+     */
+    private int gridSize;
+
+    /**
+     * Current position of the ant on the grid.
+     */
+    private Position position;
+
+
+    Ant(int gridSize){
+        this.gridSize = gridSize;
+        initPosition();
+    }
+
+    /**
+     * Set initial position on the grid.
+     */
+    protected void initPosition() {
+        // default: random place todo override in queen
+        this.position = Position.getRandomPosition(gridSize);
+    }
+
+    /**
+     * Getter for the ant's current position on the grid.
+     */
+    public Position getPosition() {
+        return position;
+    }
+
+    /**
+     * Update the ant's position (one step).
+     */
+    protected abstract void moveStep();
+
+    /**
+     * Returns the ant's distance from the queen.
+     */
+    protected int queenDist() {
+        // TODO
+        return 0;
+    }
+
 }
